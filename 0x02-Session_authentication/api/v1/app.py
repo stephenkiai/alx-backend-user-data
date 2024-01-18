5b1408e8accba69775eac4b26ebbd2a3a51cac74
stephenkiai <stephenkiai23@gmail.com>
2024-01-18 21:27:42 +0300

    pycodestyle fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.session_exp_auth import SessionExpAuth # Import the SessionExpAuth class
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
